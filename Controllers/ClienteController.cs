c15b189788305d90b60a8e45e2583249abdccbb2
FelixVillanuevaCondori <felixeduardobx@gmail.com>
2026-02-18 14:37:22 -0300

    Rotas criadas (Get/GetId/Post)

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -1,16 +1,51 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BarberAPI.Data;
+using BarberAPI.DTO;
+using BarberAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BarberAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ClienteController : ControllerBase
+    public class ClienteController : ControllerBase 
     {
+        public readonly ApplicationDbContext _dbContext;
+
+        public ClienteController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         [HttpGet]
         public async Task<IActionResult> GetClientes()
         {
             return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetClientesPorId(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> CriarClientes([FromBody] ClienteDTO clienteDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                Cliente cliente = new Cliente();
+                cliente.Nome = clienteDTO.Nome;
+                cliente.Telefone = clienteDTO.Telefone;
+                cliente.Email = clienteDTO.Email;
+                cliente.DataNascimento = clienteDTO.DataNascimento;
+
+                _dbContext.Clientes.Add(cliente); 
+                await _dbContext.SaveChangesAsync();
+                return CreatedAtAction(nameof(GetClientesPorId), new { id = cliente.ClienteId }, cliente);  
+            }
+            return BadRequest();
         }
     }
 }
---
+++ DTO/ClienteDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarberAPI.DTO
+{
+    public class ClienteDTO
+    {
+        public int ClienteId { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Telefone { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Favor digitar um email válido")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Data de Nascimento")]
+        public DateTime DataNascimento { get; set; }
+    }
+}
