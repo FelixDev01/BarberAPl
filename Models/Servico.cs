a382263b61adc5ee63612d5b57c1d0efc04e407f
FelixVillanuevaCondori <felixeduardobx@gmail.com>
2026-02-18 09:53:34 -0300

    Atualiza model Servico com DataAnnotations

--- Models/Servico.cs
+++ Models/Servico.cs
@@ -1,17 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BarberAPI.Models
 {
     public class Servico
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "O nome do serviço é obrigatório.")]
+        [Display(Name = "Nome do Serviço")]
         public string NomeServico { get; set; }
 
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
 
+        [Range(10, 999, ErrorMessage = "A duração mínima deve ser de 10 minutos até 999 minutos.")]
         public int DuracaoMin { get; set; }
 
+        [Range(0.01, 999.00, ErrorMessage = "O preço deve ficar entre 0.01 até 999.00.")]
         public decimal Preco { get; set; }
     }
 
