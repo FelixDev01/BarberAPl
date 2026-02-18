78d35830f4ada07de2e7182370e6990fbf6175df
FelixVillanuevaCondori <felixeduardobx@gmail.com>
2026-02-18 09:43:26 -0300

    Remove arquivos do Visual Studio e ajusta .gitignore

---
+++ Models/Servico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BarberAPI.Models
+{
+    public class Servico
+    {
+        public int Id { get; set; }
+
+        public string NomeServico { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int DuracaoMin { get; set; }
+
+        public decimal Preco { get; set; }
+    }
+
+}
