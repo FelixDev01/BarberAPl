79e139589e84848269c1636bbb2a1ac9e4384067
FelixVillanuevaCondori <felixeduardobx@gmail.com>
2026-02-18 10:02:09 -0300

    Agendamento e Cliente Configurados com DataAnnotations

---
+++ Models/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BarberAPI.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int ClienteId { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Telefone { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Favor digitar um email válido")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Data de Nascimento")]
+        public DateTime DataNascimento { get; set; }
+    }
+
+}
