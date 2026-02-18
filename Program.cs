a0c741343da6fc12b91ab7ebe10d9212c2b072e0
FelixVillanuevaCondori <felixeduardobx@gmail.com>
2026-02-18 12:48:06 -0300

    Configurando connectionString e o serviço do DbContext

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
+using BarberAPI.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+
+string sqlServer = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(sqlServer));
 
 var app = builder.Build();
 
