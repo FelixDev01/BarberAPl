8c67c613732b334e902c8b3e3759cbff8c3c0278
FelixVillanuevaCondori <felixeduardobx@gmail.com>
2026-02-19 18:55:08 -0300

    CRUD realizado com sucesso

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -18,19 +18,6 @@ namespace BarberAPI.Controllers
             _dbContext = dbContext;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> GetClientes()
-        {
-            var clientes = await _dbContext.Clientes.ToListAsync();
-            return Ok();
-        }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetClientesPorId(int id)
-        {
-            return Ok();
-        }
-
         [HttpPost]
 
         public async Task<IActionResult> CriarClientes([FromBody] ClienteDTO clienteDTO)
@@ -43,11 +30,70 @@ namespace BarberAPI.Controllers
                 cliente.Email = clienteDTO.Email;
                 cliente.DataNascimento = clienteDTO.DataNascimento;
 
-                _dbContext.Clientes.Add(cliente); 
+                _dbContext.Clientes.Add(cliente);
                 await _dbContext.SaveChangesAsync();
-                return CreatedAtAction(nameof(GetClientesPorId), new { id = cliente.ClienteId }, cliente);  
+                return CreatedAtAction(nameof(GetClientesPorId),new { id = cliente.ClienteId }, cliente);
             }
             return BadRequest();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetClientes()
+        {
+            var clientes = await _dbContext.Clientes.ToListAsync();
+            return Ok(clientes);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetClientesPorId(int id)
+        {
+            var cliente = await _dbContext.Clientes.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return Ok(cliente);
+        }
+
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> AtualizarCliente(int id, [FromBody] ClienteDTO clienteDTO)
+        {
+            if(id != clienteDTO.ClienteId)
+            {
+                return BadRequest();
+            }
+       
+            if (ModelState.IsValid)
+            {
+                var clienteExistente = await _dbContext.Clientes.FindAsync(clienteDTO.ClienteId);
+                if (clienteExistente == null)
+                {
+                    return NotFound();
+                }
+                clienteExistente.Nome = clienteDTO.Nome;
+                clienteExistente.Telefone = clienteDTO.Telefone;
+                clienteExistente.Email = clienteDTO.Email;
+                clienteExistente.DataNascimento = clienteDTO.DataNascimento;
+                _dbContext.Clientes.Update(clienteExistente);
+                await _dbContext.SaveChangesAsync();
+                return Ok(clienteExistente);
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeletarCliente(int id) 
+        {
+            var cliente = await _dbContext.Clientes.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Clientes.Remove(cliente);
+            await _dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+
     }
 }
