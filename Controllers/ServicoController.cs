ef6d3215d166423c1b0808241ca8b472bc647e45
FelixVillanuevaCondori <felixeduardobx@gmail.com>
2026-02-20 14:47:02 -0300

    Controller do Servico criado com Sucesso

---
+++ Controllers/ServicoController.cs
@@ -0,0 +1,95 @@
+﻿using BarberAPI.Data;
+using BarberAPI.DTO;
+using BarberAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicoController : ControllerBase
+    {
+
+        public readonly ApplicationDbContext _dbContext;
+
+        public ServicoController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarServico(ServicoDTO servicoDTO)
+        {
+            Servico servico = new Servico();
+            servico.NomeServico = servicoDTO.NomeServico;
+            servico.Descricao = servicoDTO.Descricao;
+            servico.DuracaoMin = servicoDTO.DuracaoMin;
+            servico.Preco = servicoDTO.Preco;
+            
+            _dbContext.Add(servico);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetServicoPorId), new { id = servico.Id }, servico);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetServicos()
+        {
+            var servicos = await _dbContext.Servicos.ToListAsync();
+            return Ok(servicos);
+        }
+
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetServicoPorId(int id)
+        {
+            var servico = await _dbContext.Servicos.FindAsync(id);
+            if (servico == null)
+            {
+                return NotFound();
+            }
+            return Ok(servico);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> AtualizarServico(int id, ServicoDTO servicoDTO)
+        {
+            if (id != servicoDTO.Id)
+            {
+                return BadRequest();
+            }
+    
+            var servicoExistente = await _dbContext.Servicos.FindAsync(id);
+
+            if (servicoExistente == null)
+            {
+                return NotFound();
+            }
+
+            servicoExistente.NomeServico = servicoDTO.NomeServico;
+            servicoExistente.Descricao = servicoDTO.Descricao;
+            servicoExistente.DuracaoMin = servicoDTO.DuracaoMin;
+            servicoExistente.Preco = servicoDTO.Preco;
+            await _dbContext.SaveChangesAsync();
+            return Ok(servicoExistente);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeletarServico(int id)
+        {
+            var servico = await _dbContext.Servicos.FindAsync(id);
+            if (servico == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Servicos.Remove(servico);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+    }
+
+}
---
+++ DTO/ServicoDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarberAPI.DTO
+{
+    public class ServicoDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do serviço é obrigatório.")]
+        [Display(Name = "Nome do Serviço")]
+        public string NomeServico { get; set; }
+
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        [Range(10, 999, ErrorMessage = "A duração mínima deve ser de 10 minutos até 999 minutos.")]
+        public int DuracaoMin { get; set; }
+
+        [Range(0.01, 999.00, ErrorMessage = "O preço deve ficar entre 0.01 até 999.00.")]
+        public double Preco { get; set; }
+    }
+}
